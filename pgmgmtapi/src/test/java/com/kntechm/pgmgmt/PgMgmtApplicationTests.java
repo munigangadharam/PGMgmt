815033f2af5815914af17c08ae771bbd569d0f40
Muni M <tech.munim@gmail.com>
2019-11-03 11:27:22 +0530

    Initial Code Commit

---
+++ pgmgmtapi/src/test/java/com/kntechm/pgmgmt/PgMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kntechm.pgmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
